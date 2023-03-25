97302b9ee4e026ec1519298b0d34eb909b8f1a55
Asif Raza <90905808+asifraza03@users.noreply.github.com>
2023-03-25 18:10:19 +0530

    Add files via upload
    Solve Problem Statement on Binary Search-in these find first and last index  of same no.

---
+++ three.cpp
@@ -0,0 +1,69 @@
+
+
+#include<iostream>
+using namespace std;
+
+
+int firstOccur(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+int lastOcc(int  arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+
+
+
+int main() { 
+
+    int even[12] = {2,4,4,4,4,4,4,4,4,4,4,18};
+  
+
+   
+
+    int first = firstOccur(even, 12, 4);
+    int last = lastOcc(even,12,4);
+
+    cout << " first occ " << first<<" and last  is on "<<last << endl;
+
+
+    return 0;
+}
